d079b0c3ec6a8540cb70db773b695a9d70abfd4f
vaishnavigopika <pgopikavaishnavi.10a@gmail.com>
2026-02-10 15:25:56 +0530

    user 2

--- user.py
+++ user.py
@@ -1,12 +1,30 @@
+import csv
 while True:
+    
     print("1.Register/n2.Login/n3.Exit")
     ch=int(input("enter your choice"))
     if ch==1:
         name=input("enter your name")
         password=input("enter your password")
-        with open("user.txt","a") as f:
+        with open("user.csv ","a") as f:
             f.write(name+","+password+"\n")
         print("Registration successful")
     if ch==2:
         name=input("enter your name")
+        passs= input("enter your passwrd")
+        data={}
+        with open("user.csv",r) as f1:
+            csvFile = csv.reader(file)
+            headers = next(csvFile) 
+            for h in headers:
+                data[h] = []
+            for row in csvFile: 
+                for i in range(len(headers)):
+                    data[headers[i]].append(row[i])
+        print(data)
+        for key,value in data.items():
+            if name in value and passs in value:
+                print("Login successful")
+                break
         
+            
