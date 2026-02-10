46c998a0409cba34e7e9659c6155d1b60565f7c3
vaishnavigopika <pgopikavaishnavi.10a@gmail.com>
2026-02-10 11:33:37 +0530

    exception

--- exception.py
+++ exception.py
@@ -1,10 +1,12 @@
 try:
     a= int(input("enter a number"))
     b=input("enter a string")
-    s=0
-    s=b+a
-    print(s)
+    print("The age of",b,"is",a)
 except ValueError:
     print("Invalid input. Please enter a valid integer.")
 except TypeError:
     print("Invalid input. Please enter a valid string")
+else:
+    print("input was valid and code run")
+finally:
+    print("This block will always execute, regardless of exceptions.")
